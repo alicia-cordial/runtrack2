7b38d2df9131d0c1ff9fb2046f6abed2588721fc
alicia-cordial <71872114+alicia-cordial@users.noreply.github.com>
2020-11-06 09:51:33 +0100

    JOUR 4 DERNIERE MISE A JOUR

--- jour04/job07/index.php
+++ jour04/job07/index.php
@@ -1,19 +1,68 @@
+<form action="index.php" method="post">
+
+        <input type="text" name="largeur" /><br>
+        <input type="text" name="hauteur" /><br>
+
+        <button type="submit" class="button">Valider</button>
+
+</form>
+
+
+<table>
+
+        <tbody>
+
+                <?php
+
+                $hauteur=5; 
+                $largeur=10;
+
+                        for ($line=1; $line <= $hauteur ; $line++){
+                                echo "<tr>";
+                                for ($cell=1; $cell <= $largeur ; $cell++){
+                                        if ($cell + $line <= $largeur/2 || $cell - $line > $largeur/2){
+                                                echo "<td>&nbsp</td>";
+                                        }
+
+                                        elseif ($cell + $line == $largeur/2 +1){
+                                                echo "<td>/</td>";
+                                        }
+
+                                        elseif ($cell - $line == $largeur/2){
+                                                echo "<td>\</td>";
+                                        }
+
+                                        else{
+                                                echo "<td>_</td>";
+                                        }
+                                }
+                                echo "</tr>";
+                        }
+
+                        for ($line=1; $line <=$hauteur; $line++){
+                                echo"<tr>";
+                                for ($cell=1; $cell <= $largeur ; $cell++){
+                                        if ($cell ==1 || $cell == $largeur){
+                                                echo "<td>|</td>";
+                                        }
+                                        elseif ($line == $hauteur){
+                                                echo "<td>_</td>";
+                                        }
+                                        else{
+                                                echo "<td>&nbsp</td>";
+                                        }
+                                }
+                        }
+
+
+                ?>
+        
+        
+        
+        
+        </tbody>
 
 
 
 
-
-
-
-<form method="post" action="index.php">
-
-
-        <label for= "login">LARGEUR</label> 
-        <input type="text" name="largeur" id="largeur"><br />
- 
-        <label for= "login">LONGUEUR</label> 
-        <input type="text" name="longueur" id="longueur"><br />
-
-        <input type="submit" name="submit" value="submit"><br />
-
-    </form>
+</table>
